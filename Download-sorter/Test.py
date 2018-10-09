e206b996501b56c9a76a2a3b6988afef601aeb1a
antimlgpro <ludvigvonreis@gmail.com>
2018-10-09 20:27:26 +0200

    Update Test.py

--- Download-sorter/Test.py
+++ Download-sorter/Test.py
@@ -4,26 +4,26 @@ filenamepos = ""
 yourname = "Yourname"
 for file in os.listdir(r"C:\Users\Yourname\Downloads"):
     if file.endswith(".exe"):
-        filename = os.path.join(r"C:\Users\" + yourname + "\Downloads", file)
-        filenamepos = os.path.join(r"C:\Users\" + yourname + "\Downloads\exe", file)
+        filename = os.path.join(r"C:\Users\" + yourname + "\\Downloads", file)
+        filenamepos = os.path.join(r"C:\Users\" + yourname + "\\Downloads\exe", file)
         os.rename(filename, filenamepos)
     if file.endswith(".docx"):
-        filename = os.path.join(r"C:\Users\" + yourname + "\Downloads", file)
-        filenamepos = os.path.join(r"C:\Users\" + yourname + "\Downloads\text", file)
+        filename = os.path.join(r"C:\Users\" + yourname + "\\Downloads", file)
+        filenamepos = os.path.join(r"C:\Users\" + yourname + "\\Downloads\text", file)
         os.rename(filename, filenamepos)
     if file.endswith(".png"):
-        filename = os.path.join(r"C:\Users\" + yourname + "\Downloads", file)
-        filenamepos = os.path.join(r"C:\Users\" + yourname + "\Downloads\images", file)
+        filename = os.path.join(r"C:\Users\" + yourname + "\\Downloads", file)
+        filenamepos = os.path.join(r"C:\Users\" + yourname + "\\Downloads\images", file)
         os.rename(filename, filenamepos)
     if file.endswith(".jpg"):
-        filename = os.path.join(r"C:\Users\" + yourname + "\Downloads", file)
-        filenamepos = os.path.join(r"C:\Users\" + yourname + "\Downloads\images", file)
+        filename = os.path.join(r"C:\Users\" + yourname + "\\Downloads", file)
+        filenamepos = os.path.join(r"C:\Users\" + yourname + "\\Downloads\images", file)
         os.rename(filename, filenamepos)
     if file.endswith(".gif"):
-        filename = os.path.join(r"C:\Users\" + yourname + "\Downloads", file)
-        filenamepos = os.path.join(r"C:\Users\" + yourname + "\Downloads\images", file)
+        filename = os.path.join(r"C:\Users\" + yourname + "\\Downloads", file)
+        filenamepos = os.path.join(r"C:\Users\" + yourname + "\\Downloads\images", file)
         os.rename(filename, filenamepos)
     if file.endswith(".tiff"):
-        filename = os.path.join(r"C:\Users\" + yourname + "\Downloads", file)
-        filenamepos = os.path.join(r"C:\Users\" + yourname + "\Downloads\images", file)
+        filename = os.path.join(r"C:\Users\" + yourname + "\\Downloads", file)
+        filenamepos = os.path.join(r"C:\Users\" + yourname + "\\Downloads\images", file)
         os.rename(filename, filenamepos)
